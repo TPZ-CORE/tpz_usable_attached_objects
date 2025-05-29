8f7fa5d21ed1fc607f448660e17184ce5d4e253f
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-29 14:40:35 +0300

    Update tp-client_commands.lua

--- tpz_attached_objects/client/tp-client_commands.lua
+++ tpz_attached_objects/client/tp-client_commands.lua
@@ -2,6 +2,7 @@
 --[[ Commands ]]--
 ---------------------------------------------------------------
 
+-- We add the command to the chat suggestion for didplaying it properly.
 TriggerEvent("chat:addSuggestion", "/" .. Config.DeleteAllAttached.Command, Config.DeleteAllAttached.Description)
 
 -- Removing all the usable wearable props
