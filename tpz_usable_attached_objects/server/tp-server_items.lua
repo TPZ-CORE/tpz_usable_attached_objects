1102eaba8cfd0314c61f769e2a57706e76bcb423
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-29 23:12:08 +0300

    Add files via upload

---
+++ tpz_usable_attached_objects/server/tp-server_items.lua
@@ -0,0 +1,21 @@
+
+local TPZInv = exports.tpz_inventory:getInventoryAPI()
+
+---------------------------------------------------------------
+--[[ Items ]]--
+---------------------------------------------------------------
+
+Citizen.CreateThread(function()
+
+	for item, data in pairs (Config.AttachedItemSets) do
+
+		TPZInv.registerUsableItem(item, "tpz_attached_objects", function(data)
+			local _source = data.source
+
+			TriggerClientEvent("tpz_attached_objects:client:attach", _source, item)
+			TriggerClientEvent('tpz_inventory:closePlayerInventory', _source)
+		end)
+
+	end
+
+end)
